46de9d2b3e84e230c62093611e2b1ac98d2a1a7d
nleant_nlant <88565552+nleantnlant@users.noreply.github.com>
2023-04-19 04:58:22 -0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -432,10 +432,10 @@ function Library:create(options)
 	end
 
 	options = self:set_defaults({
-		Name = "Mercury",
+		Name = "Donut Hub",
 		Size = UDim2.fromOffset(600, 400),
 		Theme = self.Themes[settings.Theme],
-		Link = "https://github.com/deeeity/mercury-lib"
+		Link = "https://github.com/nleantnlant/donut-hub-library"
 	}, options)
 
 	if getgenv and getgenv().MercuryUI then
