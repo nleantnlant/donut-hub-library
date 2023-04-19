0f3db1bd407d5df0e659b380398b368a7ddf8734
nleant_nlant <88565552+nleantnlant@users.noreply.github.com>
2023-04-19 11:38:02 -0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -1811,7 +1811,7 @@ function Library:button(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(1, -11, 0.5, 0),
 		Size = UDim2.fromOffset(26, 26),
-		Image = "rbxassetid://8498776661",
+		Image = (options.Image or "rbxassetid://8498776661")
 		Theme = {ImageColor3 = "Tertiary"}
 	})
 
